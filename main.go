30106fc70abeb323819e9a05874fe860a51012d9
shixiaohao <531845606@qq.com>
2024-07-06 18:02:18 +0800

    remove test code

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	strUtil "github.com/OrangeDrk/go-toolbox/str"
-	"strings"
-)
-
 func main() {
 
-	str := "石晓浩176"
-	index := strings.Index(str, "7")
-	fmt.Println(index)
-
-	indexOf := strUtil.IndexOf("石晓浩176", '7')
-	fmt.Println(indexOf)
-
-	fmt.Println(strUtil.SubString(str, -9, 10))
-	fmt.Println(strUtil.SubString(str, 9, 10))
 }
