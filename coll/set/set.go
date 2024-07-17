1b357221fb76c1ecec90342767074b4c77bf3590
shixiaohao <531845606@qq.com>
2024-07-17 20:16:40 +0800

    添加Set类型和简单的方法

--- coll/set/set.go
+++ coll/set/set.go
@@ -1,1 +1,46 @@
 package setUtil
+
+// 定义Set类型，底层使用map
+type Set[T comparable] map[T]struct{}
+
+// NewSet 创建并返回一个新的 Set
+func NewSet[T comparable]() Set[T] {
+	return make(Set[T])
+}
+
+// Add 向 Set 中添加元素
+func (s Set[T]) Add(elem T) {
+	s[elem] = struct{}{}
+}
+
+// Remove 从 Set 中删除元素
+func (s Set[T]) Remove(elem T) {
+	delete(s, elem)
+}
+
+// Contains 检查 Set 中是否包含指定元素
+func (s Set[T]) Contains(elem T) bool {
+	_, exists := s[elem]
+	return exists
+}
+
+// Size 返回 Set 的大小
+func (s Set[T]) Size() int {
+	return len(s)
+}
+
+// Clear 清空 Set
+func (s Set[T]) Clear() {
+	for k := range s {
+		delete(s, k)
+	}
+}
+
+// ToSlice 将 Set 转换为切片
+func (s Set[T]) ToSlice() []T {
+	keys := make([]T, 0, len(s))
+	for key := range s {
+		keys = append(keys, key)
+	}
+	return keys
+}
---
+++ coll/set/set_test.go
@@ -0,0 +1,33 @@
+package setUtil
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	// 创建一个整数类型的 Set
+	intSet := NewSet[int]()
+	intSet.Add(1)
+	intSet.Add(2)
+	intSet.Add(3)
+	fmt.Println("Int Set:", intSet.ToSlice()) // 输出: [1 2 3]
+
+	intSet.Remove(2)
+	fmt.Println("Contains 2:", intSet.Contains(2)) // 输出: false
+	fmt.Println("Size:", intSet.Size())            // 输出: 2
+
+	// 创建一个字符串类型的 Set
+	stringSet := NewSet[string]()
+	stringSet.Add("apple")
+	stringSet.Add("banana")
+	stringSet.Add("cherry")
+	fmt.Println("String Set:", stringSet.ToSlice()) // 输出: [apple banana cherry]
+
+	stringSet.Remove("banana")
+	fmt.Println("Contains 'banana':", stringSet.Contains("banana")) // 输出: false
+	fmt.Println("Size:", stringSet.Size())                          // 输出: 2
+
+	stringSet.Clear()
+	fmt.Println("Cleared String Set, Size:", stringSet.Size()) // 输出: 0
+}
